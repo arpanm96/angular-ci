26acf8dbe19053a4658f90102087b869d87415d7
Arpan Majumder <arpan.majumdar@thoughtworks.com>
2019-08-02 14:43:13 +0530

    Remove build.sourcesDirectory path
    Signed-off-by: Arpan Majumder <arpan.majumdar@thoughtworks.com>

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+echo Setting git configuration
+git config --global user.email "arpan.majumder54@gmail.com"
+git config --global user.name "Arpan Majumder"
+
 echo Git Status
 git status
 git fetch
 
-path=$build.sourcesDirectory
-echo Currently inside $path
+# path=$build.sourcesDirectory
+# echo Currently inside $path
 
-sourcePath="$path/TestApp/dist/test-app"
-echo Adding $sourcePath to git
-git add $sourcePath
+path="TestApp/dist/test-app/"
+mkdir deploy && cp -rf $path deploy/
+echo Adding deploy/ to git
+git add deploy/
 
 git commit -m "Update application"
 
 branch=gh-pages
-echo Pushing $sourcePath to $branch
+echo Pushing $path to $branch
 git push origin $branch
