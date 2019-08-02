2b29da8f1c87effdd84819f62c1db6a668dfc4e0
Arpan Majumder <arpan.majumdar@thoughtworks.com>
2019-08-02 13:43:35 +0530

    Change path
    Signed-off-by: Arpan Majumder <arpan.majumdar@thoughtworks.com>

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,16 @@
 echo Git Status
 git status
 git fetch
-path=$(build.sourcesDirectory)
+
+path=$build.sourcesDirectory
 echo Currently inside $path
+
 sourcePath="$path/TestApp/dist/test-app"
 echo Adding $sourcePath to git
 git add $sourcePath
+
 git commit -m "Update application"
+
 branch=gh-pages
 echo Pushing $sourcePath to $branch
 git push origin $branch
